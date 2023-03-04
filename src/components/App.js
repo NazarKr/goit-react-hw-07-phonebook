949bac78db31b723f1da69ef2570abdc31553916
NazarKr <nazarok1990@gmail.com>
2023-03-04 18:27:00 +0200

    upd app

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,20 @@
-import { useSelector } from 'react-redux';
 import { Layout } from './Layouy/Layout';
 import { AppBar } from 'components/AppBar/AppBar';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { FilterForm } from './FilterForm/FilterForm';
 import { ContactList } from './ContactList/ContactList';
-import { getContacts } from 'redux/selectors';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from 'redux/store';
 
 import { ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
-  const contacts = useSelector(getContacts);
   return (
-    <PersistGate loading={null} persistor={persistor}>
+    <>
       <Layout>
         <AppBar />
         <ContactForm />
         <FilterForm />
-        {contacts.length > 0 && <ContactList />}
+        <ContactList />
       </Layout>
 
       <ToastContainer
@@ -30,6 +25,6 @@ export const App = () => {
         draggable
         Transition="zoom"
       />
-    </PersistGate>
+    </>
   );
 };
